e67fc6d21ded894b882951305250d8c858624a0c
Tajwar <tajwaraashab@gmail.com>
2021-01-21 11:09:20 +0600

    authentication and authorization

--- Login-Register/login.js
+++ Login-Register/login.js
@@ -1,19 +1,27 @@
+require('dotenv').config();
 const { authSchema } = require('../validation/validation.js');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models/models.js');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const e = require('express');
 const router = express.Router();
 
-// find a way to limit the number of times a user tries to authenticate
+router.get('/', function(req, res)  {
+    res.send(req.cookies.token);
+})
 
+/**
+ * WARNING!! NO REQUEST LIMITER HAS BEEN ADDED!!!!
+ * 
+ */
 
 // log in a user
 router.post('/', async (req, res) => {
     // validate body
     try {
         const authResult = await authSchema.validateAsync(req.body);
-        // destructure
+        // destructure validated request (which is the authResult)
         const { email, password } = authResult;
         
         // find if there is a user with this email
@@ -27,33 +35,79 @@ router.post('/', async (req, res) => {
                 var cookie = req.cookies.counter;
                 if (cookie == undefined) {
                     // no cookies exsit, assign cookies
-                    var counter = 0
-                    res.cookie('counter', counter, { maxAge: 30*24*60*60*1000, httpOnly: true });
+                    //var counter = 0
+                    //res.cookie('counter', counter, { maxAge: 30*24*60*60*1000, httpOnly: true });
                     res.json({ "Error": "No user of this email has been found" });
                 }
                 else {
                     // counter cookie exists, so increasing its count
-                    counter += 1;
-                    res.cookie('counter', counter, {maxAge:30*24*60*60*1000, httpOnly: true });
+                    //counter += 1;
+                    //res.cookie('counter', counter, {maxAge:30*24*60*60*1000, httpOnly: true });
                     res.json({ "Error": "No user of this email has beend found" });
                 }
              }
              else {
                  bcrypt.compare(password, user.password, (err, isMatch) => {
                      if (isMatch) {
-                        // authorize the user
+                        // email and password matched
+                        // delete the counter cookie, if there is any
+                        // var counter = req.cookies.counter;
+                        // if (counter != undefined) {
+                        //     // clear the 'counter' cookie
+                        //     res.clearCookie('counter');
+                        // }
+                        // proceed to authorization
+                        // take the name, email, firstName and LastName and id 
+                        // and store them in an object
+                        // the use that object to sign the token
+                        const userToken = { name: user.firstName, email: user.email, _id: user._id };
+                        // now sign the token and authorize
+                        const token = jwt.sign(userToken, process.env.TOKEN_SECRET, { expiresIn: '604800s' });
+                        // now store the token in a cookie
+                        res.cookie('token', token, { maxAge: 30*1000, httpOnly: true })
+                        .send('cookies set');
+                        // should redirect to home
+
+
                      }
                      else {
                         // the password did not match
+                        res.json({"Error": "Password did not match!"});
+                        
                         // attach a cookie, if a cookie already exists, increase its count
+
+
+                        /*
+                        var cookie = req.cookies.counter;
+                        if (cookie == undefined) {
+                            // no cookies exsit, assign cookies
+                            var counter = 0
+                            res.cookie('counter', counter, { maxAge: 30*24*60*60*1000, httpOnly: true });
+                            res.json({ "Error": "No user of this email has been found" });
+                        }
+                        else {
+                            // counter cookie exists, so increasing its count
+                            counter += 1;
+                            res.cookie('counter', counter, {maxAge:30*24*60*60*1000, httpOnly: true });
+                            res.json({ "Error": "No user of this email has beend found" });
+                        }
+
+                        */
                      }
                  })
              }
          })
-         .catch()    
+         .catch((err) => {
+            // log the error in a specific log file
+
+            // send the error message
+            res.json({ "Error": err });
+         })    
     }
     catch (error) {
         // log the error in a log file
+
+        // send the error messaeg
         res.json({ "Error": error })
     }
 })
--- server.js
+++ server.js
@@ -5,18 +5,66 @@ const helmet = require('helmet');
 const registerRouter = require('./Login-Register/register.js');
 const loginRouter = require('./Login-Register/login.js');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(cookieParser());
 app.use(helmet());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(process.env.PORT || 3000, () => { console.log('Mongodb connected; Server is listening')}))
+    .then((result) => app.listen(process.env.PORT || 3000, () => { console.log('Mongodb connected; Server is listening on Port 3000')}))
     .catch((err) => console.log(err))
 
 app.use(express.json())
-app.use('/api/register', registerRouter);
-app.use('/api/login', loginRouter);
+app.use('/api/register', isloggedIn, registerRouter);
+app.use('/api/login', isloggedIn, loginRouter);
 
 
+// profile route
+app.get('/profile', cookieCheck, authCheck, (req, res) => {
+    res.send(req.user);
+})
 
+/*
+// create a logout route here
+app.delete('/logout', (req, res) => {
+    res.writeHead(200, {
+        "Set-Cookie": `token=${token}; HttpOnly; expires=${new Date(new Date().getTime()+0 * 60000).toUTCString()}`,
+        "Access-Control-Allow-Credentials": "true"
+    }).send()
+});
+*/
+
+// MIDDLEWARES
+
+// middlware to check cookies
+function cookieCheck (req, res, next) {
+    if (req.cookies.token == undefined) {
+        // action to take for absence of cookies
+        res.sendStatus(403);
+    }
+    else {
+        next();
+    }
+}
+
+// middleware to authorize
+function authCheck(req, res, next) {
+    jwt.verify(req.cookies.token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(401);
+        else {
+            req.user = user;
+            next();
+        }
+    })
+}
+
+// middlware for login check
+function isloggedIn(req, res, next) {
+    if (!req.user) {
+        next();
+    }
+    else {
+        res.redirect('/');
+    }
+}
